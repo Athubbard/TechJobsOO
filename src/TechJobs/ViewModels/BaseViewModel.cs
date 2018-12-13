8a4a382d4599ce5936bdfeb337bc84a64389dd14
Amber Hubbard <amberthubbard@gmail.com>
2018-12-12 20:31:42 -0600

    Finished product

---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+
+    public class BaseViewModel
+    {
+        public List<JobFieldType> Columns { get; set; } //display all columns
+
+        public string Title { get; set; } = ""; //title view
+
+        public BaseViewModel()
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType))) //all columns list
+            {
+                Columns.Add(enumVal);
+            }
+
+        }
+    }
+}
